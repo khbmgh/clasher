bd863965a833088c6309b3290bc3e3d9b8451a43
khbmgh <mr.afzoni@gmail.com>
2026-02-24 09:03:05 +0330

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,8 @@ https://raw.githubusercontent.com/expressalaki/ExpressVPN/refs/heads/main/config
 https://raw.githubusercontent.com/expressalaki/ExpressVPN/refs/heads/main/configs4.txt
 https://raw.githubusercontent.com/miladtahanian/V2RayCFGDumper/main/config.txt
 https://raw.githubusercontent.com/Mahdi0024/ProxyCollector/master/sub/proxies.txt
-https://raw.githubusercontent.com/youfoundamin/V2rayCollector/main/mixed_iran.txt`.split("\n").map(s => s.trim()).filter(Boolean))]
+https://raw.githubusercontent.com/youfoundamin/V2rayCollector/main/mixed_iran.txt
+`.split("\n").map(s => s.trim()).filter(Boolean))]
 // =====================================================
 // ۲. موتور اصلی
 // =====================================================
