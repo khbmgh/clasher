f98c8c57e99d1a695664fcd1bc2c9867cbeb55ab
khbmgh <mr.afzoni@gmail.com>
2026-07-18 11:03:05 +0330

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 // ۱. تنظیمات
 // =====================================================
 const FETCH_TIMEOUT    = 15000;
-const MAX_PER_PROTOCOL = 150;
+const MAX_PER_PROTOCOL = 250;
 
 const SUBS = [...new Set(`
 https://raw.githubusercontent.com/10ium/base64-encoder/main/encoded/10ium_proxy_configs.txt
